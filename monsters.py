624a8a1b4353fdf81a734df56e22ed8005be3b62
Michael W <welchm27@gmail.com>
2023-07-14 18:54:03 -0400

    initial build of the monster dictionary and testing out roll functions

---
+++ monsters.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+monsters = {
+    "Zombie": {
+        "health": 22,
+        "armor": 8,
+        "damage": "3d6"
+    },
+    "Skeleton": {
+        "health": 13,
+        "armor": 13,
+        "damage": "1d6"
+    },
+    "Kobold": {
+        "health": 5,
+        "armor": 12,
+        "damage": "1d4"
+    }
+}
