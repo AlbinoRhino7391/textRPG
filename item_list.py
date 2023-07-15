6fa408bef370774fad3acdd853f0333bb543d93c
Michael W <welchm27@gmail.com>
2023-07-15 19:53:29 -0400

    Added an items dict and updated my rooms dict to include the matching items

---
+++ item_list.py
@@ -0,0 +1,12 @@
+# create list of items
+items = {
+    'key': {
+        'description': 'A small rusty key.\n \
+            it seems important',
+        'properties': 'Unlock doors'
+    },
+    'rubble': {
+        'description': 'A pile of rubble blocking the passage.',
+        'property': 'Impedes progress'
+    }
+}
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
 from michaelWork import *
+from character_classes import *
 
 roll_for_monster("Kobold")
